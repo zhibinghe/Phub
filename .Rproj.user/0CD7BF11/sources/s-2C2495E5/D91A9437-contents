d3056f64bb5bf5be61661cc3497a202dc24d1b97
hzb <zhibinghe1990@gmail.com>
2022-05-13 00:36:04 -0700

    add data

---
+++ .Rproj.user/0CD7BF11/sources/s-2C2495E5/D91A9437-contents
@@ -0,0 +1,31 @@
+#' Items of 5000 transactions in a chain of bakery stores.
+#' 
+#' A sub-sample of the Extended Bakery Dataset. The full dataset provides the transaction
+#' details in a chain of bakery stores. 
+#' 'F' in the colnames represents Food and D is for Drink.  
+#'
+#' @format A data frame with 5000 rows (transactions) and 50 variables (items):
+#' \describe{
+#'   \item{cakes}{F1--F10 belong to cakes}
+#'   \item{tarts}{F11--F20 belong to tarts}
+#'   \item{cookies}{F21--F30 belong to coolies}
+#'   \item{pastries}{F31-40 belong to pastries}
+#'   \item{drinks}{D1--D10 belong to drinks}
+#' }
+#' @source \url{http://users.csc.calpoly.edu/~dekhtyar/466-Spring2018/labs/lab01.html}
+#' 
+#' @examples 
+#' data(bakery5000)
+"bakery5000"
+
+
+#' Grouping behavior of passerines
+#' 
+#' The dataset includes 63 color-marked passerines in Australia for daily observations  
+#'
+#' @format A data frame with 109 rows and 63 variables:
+#' @source \url{https://zookeys.pensoft.net/articles.php?id=6934}
+#' 
+#' @examples 
+#' data(passerines)
+"passerines"
---
+++ R/datahub.R
@@ -0,0 +1,44 @@
+#' Data Analysis using penalized hub model
+#'
+#' @param data  observed group data, the first \eqn{M} colunmns are corresponding to hub nodes
+#' @param M size of potential hub set 
+#' @param Lam a vector of tunning parameter \eqn{\lambda} 
+#' @param bootstrap number of bootstrap times
+
+#' @return a matrix of hub nodes weight for different \code{Lam}
+#' @export
+#' @import Rsolnp
+#' @examples
+#' data(bakery5000)
+#' Lam = seq(0.015,0.035,0.005)
+#' data.hub(bakery5000,40,Lam,0)
+#' data.hub(bakery5000,40,Lam,100)
+#'   
+data.hub = function(data,M,Lam,bootstrap=0){
+  data = as.matrix(data)
+  n = ncol(data)
+  # 
+  A = matrix(NA,M,n)
+  for (i in 1:M) A[i,] = colSums(data[,i]*data)/sum(data[,i])
+  diag(A) = 1; A = rbind(runif(n),A)
+  rho = c(0.2,rep(0.8/M,M))
+  #
+  coef = matrix(NA,length(Lam),M+1)
+  colnames(coef) = paste("H",0:M,sep="")
+  rownames(coef) = paste("Lam",Lam,sep="")
+  for (i in 1:length(Lam)){
+    if (bootstrap == 0){
+      coef[i,] = (phub(data,A,rho,Lam[i],pen.type="plog")$rho > 0) + 0
+    }
+    else{
+      out = matrix(0,bootstrap,M+1)
+      for(j in 1:bootstrap){
+        sub = data[sample(1:T,T,replace=T),]
+        out[j,] = (phub(sub,A,rho,Lam[i],pen.type="plog")$rho > 0) + 0 
+      }
+      coef[i,] = colMeans(out)
+    }
+  }
+  return(coef)
+}
+
